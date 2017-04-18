af200a18b27a25cc3d80fe7c032190c498d6de22
yi.chen <chenyi19890902@gmail.com>
2017-04-19 00:23:12 +0200

    remove other cout than rmse

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -70,7 +70,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     if (!is_initialized_) {
  
     // first measurement
-    cout << "EKF: " << endl;
+    //cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 0, 0;
 
@@ -140,6 +140,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   }
 
   // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
+  //cout << "x_ = " << ekf_.x_ << endl;
+  //cout << "P_ = " << ekf_.P_ << endl;
 }
